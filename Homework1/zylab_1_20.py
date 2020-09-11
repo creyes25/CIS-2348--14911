d0e6955cae9e1b4c0f3e241a6061290bec80c13c
creyes25 <70359065+creyes25@users.noreply.github.com>
2020-09-11 16:18:42 -0500

    Add files via upload
    Zylab 1.20

---
+++ Homework1/zylab_1_20.py
@@ -0,0 +1,22 @@
+
+# Carolina Reyes
+# 1563200
+
+user_num1 = int(input('Enter integer:\n'))
+
+
+Square_num1 = user_num1 * user_num1  # will cube the input that the user inputs
+
+cubed_num1 = user_num1 * user_num1 * user_num1  # multiply the user input
+
+print('You entered:', user_num1)  # displays the integer user input
+print(user_num1, 'squared is', Square_num1)  # prints the total (squared)
+print('And', user_num1, 'cubed is', cubed_num1, '!!')  # prints the total (cubed)
+
+user_num2 = int(input('Enter another integer:\n'))  # allows user to input another integer
+
+addition = user_num1 + user_num2  # adds the first number and second number
+multiply = user_num1 * user_num2  # multiply the fist number and second number
+
+print(user_num1, '+', user_num2, 'is', addition)  # prints the sum
+print(user_num1, '*', user_num2, 'is', multiply)  # prints the multiply
