ea75c2524ee0e13cc5f4c82b5f8492b03c8bbc34
creyes25 <70359065+creyes25@users.noreply.github.com>
2020-10-06 19:22:36 -0500

    Update HW2_Coding_problem2.py
    Added comments for the reader, so they can understand the code

--- Homework 2/HW2_Coding_problem2.py
+++ Homework 2/HW2_Coding_problem2.py
@@ -2,29 +2,37 @@
 # Carolina Reyes
 # 1563200
 
+# open the file the user inputs and reads it
+# opens a new file where the output will be written
 dates_inputs = open(input(), 'r')
 dates_outputs = open('parsedDates.txt', 'w')
 
+# a array so that the Months can be identified later on
 Listings = ['January', 'February', 'March', 'April', 'May', 'June',
             'July', 'August', 'September', 'October', 'November', 'December']
 
+# if -1 appears in a any file it will stop it
 for inputs in dates_inputs:
     if inputs == '-1':
         break
 
+# it will split the inputs from the file and arrange them in order
     lists = inputs.split()
     if len(lists) >= 3:
         month = lists[0]
         day = lists[1]
-        year = lists[2]        
+        year = lists[2]
 
+# gets all the data and formats it to mm/dd/yyyy
     for months in range(12):
-        if inputs.find(Listings[months]) >=0:
+        if inputs.find(Listings[months]) >= 0:
             month_number = str(months + 1)
             day = day[:- 1]
             output_list = month_number + "/" + day + "/" + year
-            dates_outputs.write(output_list)
-            dates_outputs.write("\n")
-            
+            dates_outputs.write(output_list)  # writes the new format into the new file
+            dates_outputs.write("\n")         # adds a new line after ever output
+
+# closes both files
 dates_inputs.close()
 dates_outputs.close()
+
